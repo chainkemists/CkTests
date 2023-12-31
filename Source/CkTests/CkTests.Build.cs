2c4c1184620f1e8d004bb48f875b2899e33c3df7
Sulfur-CK <sulfur@chainkemists.com>
2023-12-31 14:38:24 -0800

    feat: moved JumpAbility (heavily refactored) from CkFoundation to CkTests

---
+++ Source/CkTests/CkTests.Build.cs
@@ -0,0 +1,27 @@
+using System.IO;
+using UnrealBuildTool;
+
+public class CkTests : CkModuleRules
+{
+    public CkTests(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PrivateIncludePaths.AddRange(new string[] {
+            // ... add other private include paths required here ...
+        });
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+
+            "CkCore",
+            "CkEcs",
+            "CkLabel",
+            "CkLog",
+            "CkRecord",
+            "CkSettings",
+            "CkSignal",
+        });
+    }
+}
---
+++ Source/CkTests/CkTests.cpp
@@ -0,0 +1,15 @@
+#include "CkTests.h"
+
+#define LOCTEXT_NAMESPACE "FCkTestsModule"
+
+void FCkTestsModule::StartupModule()
+{
+}
+
+void FCkTestsModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkTestsModule, CkTests)
