8bffa4021d62176e292985bc62ad3ac90db36a40
Sulfur-CK <sulfur@chainkemists.com>
2024-12-22 18:07:34 -0800

    feat: added Tests Only GameplayTags for IsmRenderers

--- Source/CkTests/Public/CkTests/CkTests_Fragment_Data.cpp
+++ Source/CkTests/Public/CkTests/CkTests_Fragment_Data.cpp
@@ -4,3 +4,11 @@
 
 CKTESTS_API UE_DEFINE_GAMEPLAY_TAG(TAG_Timer_Tests, "Timer.Tests.Default");
 
+// --------------------------------------------------------------------------------------------------------------------
+
+CKTESTS_API UE_DEFINE_GAMEPLAY_TAG(TAG_Ism_Renderer_Test_Cube, "Ism.Renderer.Test.Cube");
+CKTESTS_API UE_DEFINE_GAMEPLAY_TAG(TAG_Ism_Renderer_Test_Cylinder, "Ism.Renderer.Test.Cylinder");
+CKTESTS_API UE_DEFINE_GAMEPLAY_TAG(TAG_Ism_Renderer_Test_Sphere, "Ism.Renderer.Test.Sphere");
+
+// --------------------------------------------------------------------------------------------------------------------
+
--- Source/CkTests/Public/CkTests/CkTests_Fragment_Data.h
+++ Source/CkTests/Public/CkTests/CkTests_Fragment_Data.h
@@ -5,3 +5,12 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 CKTESTS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Timer_Tests);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+CKTESTS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Ism_Renderer_Test_Cube);
+CKTESTS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Ism_Renderer_Test_Cylinder);
+CKTESTS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Ism_Renderer_Test_Sphere);
+
+// --------------------------------------------------------------------------------------------------------------------
+
